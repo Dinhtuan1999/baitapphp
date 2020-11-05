26a34b8c976d35fab76a4ee596bc41555d2db5fe
Dinhtuan1999 <nguyendinhtuan112@gmail.com>
2020-11-05 17:12:46 +0700

    baitap

---
+++ bai2.php
@@ -0,0 +1,20 @@
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['btn'])) {
+    if ($_GET["User"] == "admin" && $_GET["Password"] == "admin") {
+        echo "hello";
+    } else
+        echo "bye";
+}
+
+else {
+    ?>
+    <form method="get">
+    <h1>Thanh vien C11</h1>
+    <input type="text" name="User" placeholder="Nhap vao day"> <br>
+    <input type="password" name="Password" placeholder="Nhap vao day"> <br>
+    <button type="submit" name="btn" id="submit">Dang nhap</button>
+</form>
+<?php
+}
+?>
